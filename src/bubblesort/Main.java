e7c08bae6beec7bd0f0c44254c5ce8a4bbc49b6c
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-09 09:46:54 -0500

    refactor bubblesort to add swap check

--- src/bubblesort/Main.java
+++ src/bubblesort/Main.java
@@ -2,39 +2,54 @@ package bubblesort;
 
 public class Main {
     public static void main(String[] args) {
-
         int[] nums = { 6, 5, 2, 8, 9, 4 };
-        int temp;
 
         System.out.println("before sorting");
         for (int num : nums) {
             System.out.print(num + " ");
         }
 
-        // Bubble sort -> based on swapping the values
-        // Nested loop based on the size of the array
-        // Time Complexity: O(n^2)
-        for (int i = 0; i < nums.length; i++) {
-            // since the last(i) position is sorted already
-            // nums.length - i -1
-            for (int j = 0; j < nums.length - i- 1; j++) {
-                if (nums[j] > nums[j+1]) {
-                    temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
-            }
-            System.out.println();
-
-            for (int num : nums) {
-                System.out.print(num + " ");
-            }
-        }
+       bubbleSort(nums);
 
         System.out.println();
         System.out.println("after sorting");
         for (int num : nums) {
             System.out.print(num + " ");
+        }
+    }
+
+    private static void bubbleSort(int[] nums) {
+        // Bubble sort -> based on swapping the values
+        // Simplest sorting algorithm that works by repeatedly swapping
+        // the adjacent elements if they are in the wrong order.
+        // This algorithm is not suitable for large data sets as its average
+        // and worst-case time complexity is quite high
+
+        // Traverse from left and compare adjacent elements and the higher one is
+        // placed at right side
+        // The largest element is moved to the rightmost end at first
+        // Step is repeated until the data is sorted
+        // https://www.geeksforgeeks.org/bubble-sort/
+
+        int size = nums.length;
+        int temp;
+        boolean swapped;
+
+        for (int i = 0; i < size - 1; i++) { // because last iteration contains only one number
+            swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (nums[j] > nums[j+1]) {
+                    temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                    swapped = true;
+                }
+            }
+            // if no elements are swapped during an inner loop
+            // it means that the array is already sorted
+            if (!swapped) {
+                break;
+            }
         }
     }
 }
